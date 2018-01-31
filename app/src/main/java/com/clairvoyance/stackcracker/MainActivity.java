ebbd54b639cc79d00b68c2ad8dfbd785ab6b62c5
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2018-01-30 19:44:49 -0600

    ListView Test

--- app/src/main/java/com/clairvoyance/stackcracker/MainActivity.java
+++ app/src/main/java/com/clairvoyance/stackcracker/MainActivity.java
@@ -2,13 +2,54 @@ package com.clairvoyance.stackcracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    User mainUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainUser = new User("testID");
+        ListView mainList = findViewById(R.id.mainList);
+        mainList.setAdapter(new MyAdapter());
+
     }
 
+    /**
+     * A simple array adapter
+     */
+    private class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return mainUser.getTasks().size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mainUser.getTasks().get(position).getId();
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.task_item, container, false);
+            }
+
+            ((TextView) convertView.findViewById(android.R.id.text1))
+                    .setText(getItem(position));
+            return convertView;
+        }
+    }
 }
